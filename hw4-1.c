2bd09bad842745d7f8b44eede01f620b03e7d98f
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-31 01:18:06 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+#include<stdbool.h>
+#include<assert.h>
+#define aa 50
+
+int main()
+{
+    int a,c;
+    scanf("%d",&c);
+    assert(c>=1&&c<=50);
+    bool prime[aa];
+    
+    for(int j=0;j<c;j++)
+    {
+        
+    scanf("%d",&a);
+    for(int i=2;i*i<=a;i++)
+    
+        if(a%i==0)
+        {
+            printf("NO");
+        }
+        else
+        {
+            printf("YES");
+        }
+    }
+    
+
+    return 0;
+}

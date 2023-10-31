e18a566efe59d46bdc203c92adecf3346c8bf0c9
1015wen <146065580+1015wen@users.noreply.github.com>
2023-10-31 10:25:24 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#include<stdbool.h>
-#include<assert.h>
-#define aa 50
+#include <stdlib.h>
 
 int main()
 {
-    int a,c;
-    scanf("%d",&c);
-    assert(c>=1&&c<=50);
-    bool prime[aa];
-    
-    for(int j=0;j<c;j++)
-    {
-        
-    scanf("%d",&a);
-    for(int i=2;i*i<=a;i++)
-    {
-    
-        if(a%i==0)
-        {
-            printf("NO");
-        }
-        else
-        {
-            printf("YES");
-        }
-    }
-    }
-    
+    int n,i,a;
+      
+        scanf(”%d“,&n);
 
-    return 0;
-}
+    int numbers[50];
+
+    for(i=0;i<n;i++){
+        scanf(”%d/n“,&numbers[i]);
+    }
+    for(i=0;i<n;i++){
+        int isprime=1;
+
+        for(a=2;a<numbers[i];a++){
+            if(numbers[i]%a==0){
+                isprime=0;
+                break;
+            }
+        }
+        if(isprime){
+            printf(”YES “);
+        }
+        else{
+            printf(”NO “);
+        }
+
+    }
+
+    }
